35edfb03e0c7e007e73b019dac76ec6548a251ea
SterkGame <103946077+SterkGame@users.noreply.github.com>
2025-08-26 15:12:35 +0300

    Delete scripts directory

--- scripts/ksot.bat
+++
@@ -1,7 +0,0 @@
-chcp 1251
-rd /s /q C:\Users\%USERNAME%\AppData\Local\1c
-c:
-cd \
-chcp 1251	
-cd "C:\Program Files (x86)\1cv8\8.3.10.2466\bin"
-start 1cv8.exe ENTERPRISE /S "server1304:1841/market" /N "Kasyr KSO" /P "12345"
